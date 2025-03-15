2ca4b3558f68a018005230c3f79d3105f2f48816
Roberto Bautista <44622828+rrobertobt@users.noreply.github.com>
2025-03-14 18:04:01 -0600

    init: Create Laravel project structure with base files and configurations

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+<x-empty>
+  Testing laravel with a layout
+  <button class="btn btn-primary">Click me</button>
+  <button class="btn btn-secondar">Click me</button>
+  <button class="btn btn-destructive">Click me</button>
+</x-empty>
+    
